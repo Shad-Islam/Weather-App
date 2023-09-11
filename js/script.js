4173c93a7091bed40012cb5b06e9b359d07483dc
Shad-islam <shadislam48@gmail.com>
2023-09-12 04:30:23 +0600

    add condition to display the card

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
 
-  console.log(data);
+  //   console.log(data);
 
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
@@ -27,6 +27,8 @@ async function checkWeather(city) {
   } else if (data.weather[0].main == "Mist") {
     weatherIcon.src = "./images/mist.png";
   }
+
+  document.querySelector(".weather").style.display = "block";
 }
 
 searchBtn.addEventListener("click", () => {
